a3215258506e3835125021fce6ad2cfaba76e31d
ganesh1172 <ganesh152595@gmail.com>
2020-04-16 16:31:18 +0530

    added new script inside App.js and create new file 'Note.js'

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Note from "./components/Note";
 import "./style.css";
 
 export default class App extends Component {
@@ -14,23 +15,49 @@ export default class App extends Component {
     this.setState({ noteText: noteText.target.value });
   }
 
+  addNote() {
+    if (this.state.noteText === "") {
+      return;
+    }
+    let notesArr = this.state.notes;
+    notesArr.push(this.state.noteText);
+    this.setState({ noteText: "" });
+    this.textInput.focus();
+  }
   handleKeyPress = (event) => {
     if (event.key === "Enter") {
+      let notesArr = this.state.notes;
+      notesArr.push(this.state.noteText);
+      this.setState({ noteText: "" });
     }
   };
+
+  deleteNote(index) {
+    let notesArr = this.state.notes;
+    notesArr.splice(index, 1);
+    this.setState({ notes: notesArr });
+  }
   render() {
+    let notes = this.state.notes.map((val, key) => {
+      return (
+        <Note key={key} text={val} deleteMethod={() => this.deleteNote(key)} />
+      );
+    });
     return (
       <div className="container">
         <div className="header">React Todo Application</div>
-        <div className="btn">+</div>
+        {notes}
+        <div className="btn" onClick={this.addNote.bind(this)}>
+          +
+        </div>
         <input
           type="text"
           ref={(input) => {
-            this.textInput = Input;
+            this.textInput = input;
           }}
           className="textInput"
           value={this.state.noteText}
-          onChangeText={(noteText) => this.updateNoteText(noteText)}
+          onChange={(noteText) => this.updateNoteText(noteText)}
           onKeyPress={this.handleKeyPress.bind(this)}
         />
       </div>
--- src/components/Note.js
+++ src/components/Note.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 
 export default class Note extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="note" onClick={this.props.deleteMethod}>
+        {this.props.text}
+      </div>
+    );
   }
 }
