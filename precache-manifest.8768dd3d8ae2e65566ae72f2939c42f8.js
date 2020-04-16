d7926ef8256166488ba561173375e5f59438d9f3
ganesh1172 <ganesh152595@gmail.com>
2020-04-16 13:01:48 +0530

    Updates

---
+++ precache-manifest.8768dd3d8ae2e65566ae72f2939c42f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "049aa191fc82dc2977718db1a3a8ea7b",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "d5978aedda522b8d97d1",
+    "url": "/todo-list/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a29f0c7f5b1184821fba",
+    "url": "/todo-list/static/js/2.6037fe85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.6037fe85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5978aedda522b8d97d1",
+    "url": "/todo-list/static/js/main.b27131f6.chunk.js"
+  },
+  {
+    "revision": "9c00833b61171e0ba36d",
+    "url": "/todo-list/static/js/runtime-main.c17e1442.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
